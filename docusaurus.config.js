517fd54341c3031dda59938360251c34a91e339a
Jose Uhia <jaug17mat@gmail.com>
2025-03-08 09:47:12 -0500

    Fix docusaurus.config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://vercel.com/jose-uhias-projects',
+  url: 'https://docu-saurus-ips.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
